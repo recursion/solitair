0dc5e710088099b3b9e842498625b9ee634b87f4
Michael Symmes <msymmes@gmail.com>
2019-04-25 22:14:48 -0400

    Remove old commented code and imports.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import React from "react";
-import { images } from "./images.js";
 import Table from "./Table";
 import * as cards from "./solitaire";
 
 window.cards = cards;
-/*
-  <>
-    <img src={images["AH.png"]} alt="Playing Card" width="5%" height="5%" />
-    <h1>Hi!</h1>
-  </>
-*/
 
 function App() {
   return <Table />;
