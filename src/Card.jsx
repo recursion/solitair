19301cf61e8bce24a16eae0ed10278f2e2e3c72f
Michael Symmes <msymmes@gmail.com>
2019-04-26 00:24:31 -0400

    cleaned up isSelected test.

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { images } from "./images.js";
 
+const isSelected = (card, selected) => {
+  return (
+    selected && (card.value === selected.value && card.suit === selected.suit)
+  );
+};
+
 // represents a card
 // can be face up or face down
 const Card = ({ card, clickHandler, offset, selected }) => {
   if (!card) return <div />;
-  const className =
-    selected && (card.value === selected.value && card.suit === selected.suit)
-      ? `border-4 border-blue rounded absolute}`
-      : `absolute`;
+  const className = isSelected(card, selected)
+    ? `border-4 border-blue rounded`
+    : `` + `absolute`;
   if (card.faceUp) {
     return (
       <img
