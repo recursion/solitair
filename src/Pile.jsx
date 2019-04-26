26f463d61a7505f08bf5ce5a22e98c470e27443d
Michael Symmes <msymmes@gmail.com>
2019-04-25 22:15:14 -0400

    Add key to cards.

--- src/Pile.jsx
+++ src/Pile.jsx
@@ -3,20 +3,35 @@ import Card from "./Card";
 
 const stackOffset = 30;
 
+let uid = 0;
+const getUID = () => uid++;
+
 // represents a pile of cards
 // cards will be stacked on top of each other
-const Pile = ({ cards, clickHandler }) => (
-  <div className="border-white border-2 rounded w-32 h-48 relative">
-    {cards
-      .filter(c => c.faceUp)
-      .reverse()
-      .map((card, i) => (
-        <Card
-          card={card}
-          clickHandler={clickHandler}
-          offset={i * stackOffset}
-        />
-      ))}
+const Pile = ({ cards, cardClickHandler, pileClickHandler }) => (
+  <div
+    className="border-white border-2 rounded w-32 h-48 relative"
+    onClick={cards.length === 0 ? pileClickHandler : () => {}}
+  >
+    {cards[0] && cards[0].faceUp ? (
+      cards
+        .filter(c => c.faceUp)
+        .reverse()
+        .map((card, i) => (
+          <Card
+            card={card}
+            clickHandler={cardClickHandler}
+            offset={i * stackOffset}
+            key={getUID()}
+          />
+        ))
+    ) : (
+      <Card
+        card={cards[0] ? cards[0] : null}
+        clickHandler={cardClickHandler}
+        offset={0}
+      />
+    )}
   </div>
 );
 export default Pile;
