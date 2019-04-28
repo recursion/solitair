c90b2935c717b2925883dfff2e83e8d85fcf38a9
Michael Symmes <msymmes@gmail.com>
2019-04-28 09:39:11 -0400

    Remove unused test file.

--- src/solitaire.test.js
+++
@@ -1,8 +0,0 @@
-import * as cards from "./solitaire";
-
-describe("Deck", () => {
-  it("returns a new unshuffled deck containing 52 elements", () => {
-    let deck = cards.Deck();
-    expect(deck.length === 52);
-  });
-});
