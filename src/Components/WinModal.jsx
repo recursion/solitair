5b7f68ed4274bd017ffa9f348d8c0123fef724ea
Michael Symmes <msymmes@gmail.com>
2019-04-28 10:56:48 -0400

    Fix incorrect function name restart to reset.

--- src/Components/WinModal.jsx
+++ src/Components/WinModal.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const WinModal = ({ restart }) => (
+const WinModal = ({ reset }) => (
   <div className="fixed pin-t pin-l h-screen w-full flex flex-col items-center justify-center font-sans">
     <div className="h-screen w-full absolute flex items-center justify-center bg-modal">
       <div className="bg-white border border-blue rounded shadow p-8 m-4 max-w-xs max-h-full text-center overflow-y-hidden">
@@ -12,7 +12,7 @@ const WinModal = ({ restart }) => (
         </div>
         <div className="flex justify-center">
           <button
-            onClick={restart}
+            onClick={reset}
             className="flex-no-shrink text-white py-2 px-4 rounded bg-teal hover:bg-teal-dark"
           >
             Play Again
